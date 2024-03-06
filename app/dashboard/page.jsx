361b4d459ca5949ece5f6a4740d04a33f4d9c834
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2024-03-07 04:01:46 +0600

    download button added

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,16 @@
 import { Button } from "@/components/ui/button";
-import img from '../../public/boy-pic.jpg'
+import img from "../../public/boy-pic.jpg";
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 import Link from "next/link";
+import Image from "next/image";
 
 const bookData = [
   {
@@ -46,41 +47,39 @@ const Dashboard = () => {
         <div className="border-r bg-gray-100 h-screen w-1/5">
           {linkData.map((data) => (
             <div key={data.id} className="bg-green-200 ">
-              <Link className="w-full block hover:bg-slate-500 px-6 py-3" href={data.href}>{data.label}</Link>
+              <Link
+                className="w-full block hover:bg-slate-500 px-6 py-3"
+                href={data.href}
+              >
+                {data.label}
+              </Link>
             </div>
           ))}
         </div>
 
         <div className="container mx-auto">
-          <div>
-            <Table>
-              {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px]">Number</TableHead>
-                  <TableHead>Title</TableHead>
-                  <TableHead>MB</TableHead>
-                  <TableHead className="text-right">Action</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {/* <TableCell className="font-medium">INV001</TableCell>
-                  <TableCell>Paid</TableCell>
-                  <TableCell>Credit Card</TableCell>
-                  <TableCell className="text-right">$250.00</TableCell> */}
-
-                {bookData.map((data, index) => (
-                  <TableRow key={data.id}>
-                    <TableCell>{index + 1}</TableCell>
-                    <TableCell>{data.label}</TableCell>
-                    <TableCell className="font-semibold">{data.mb}</TableCell>
-                    <TableCell className="text-right">
-                      <a href="../../public/hellopdf" download ><Button>Download now</Button></a>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+          <div className="flex gap-4 mt-8">
+            {bookData.map((data) => (
+              <div key={data.id}>
+                <Card>
+                  <CardHeader>
+                    <Image
+                      className="w-[100%] h-[15rem] object-cover rounded-lg"
+                      src={img}
+                      alt="img"
+                    />
+                    <CardTitle>{data.label}</CardTitle>
+                    <CardDescription></CardDescription>
+                  </CardHeader>
+                  <CardContent>{/* <p>{data.mb}</p> */}</CardContent>
+                  <CardFooter>
+                    <a download href="logo.svg" className="block w-full">
+                      <Button className="w-full">Dwonload now</Button>
+                    </a>
+                  </CardFooter>
+                </Card>
+              </div>
+            ))}
           </div>
         </div>
       </section>
