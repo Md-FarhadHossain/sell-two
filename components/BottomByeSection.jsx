91022f8ab3a77725242aa58747e13b1df59038dd
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2024-03-10 04:27:00 +0600

    bottom part has been done with mobile and also the desktop version

--- components/BottomByeSection.jsx
+++ components/BottomByeSection.jsx
@@ -7,25 +7,36 @@ import Image from "next/image";
 const BottomByeSection = () => {
   return (
     <section className="fixed border-t z-50 bottom-0 bg-white w-full shadow-[rgba(0,0,15,0.12)_0px_0px_20px_4px]">
-      <div className="flex items-center justify-between py-4 container mx-auto">
-        <div className="flex items-center gap-3 lg:block md:block hidden">
-          <div><Image className="w-[50px] object-contain" src={bookCover2} alt="bookCover" /></div>
-          <div className="text-xl font-semibold">একজন মুসলমানের জীবনে ২৪ ঘণ্টা</div>
+      <div className="flex items-center lg:justify-between md:justify-between justify-center py-4 lg:container md:container w-full mx-auto">
+        <div className="gap-3 lg:flex md:flex hidden items-center ">
+          <div>
+            <Image
+              className="w-[50px] object-contain"
+              src={bookCover2}
+              alt="bookCover"
+            />
+          </div>
+          <div className="text-xl font-semibold">
+            একজন মুসলমানের জীবনে ২৪ ঘণ্টা
+          </div>
         </div>
-        <div className="flex items-center">
-          <div className="flex gap-2">
+        <div className="flex items-center w-full lg:w-auto md:w-auto lg:px-0 md:px-0 px-4">
+          <div className="lg:flex md:flex hidden gap-2">
             <div className="text-4xl font-bold text-[#de1d59]">৳১৬০</div>
             <div className="line-through text-3xl font-semibold text-[#a8a8a8] mr-3">
               ৳৩২০
             </div>
           </div>
-          <button className="bg-yellow-300 text-black text-[17px] border border-yellow-500 font-semibold flex px-16 py-3 rounded-lg text-xl items-center">
-            <span>
-              {" "}
-              এখনই কিনুন <span className="font-bold">৳১৬০</span>
-            </span>
-            <Image className="w-[22px] ml-2" src={rocket} alt="rocket" />
-          </button>
+          <div className="w-full">
+            {" "}
+            <button className="bg-yellow-300 w-full justify-center text-black text-[17px] border border-yellow-500 font-semibold flex px-16 py-3 rounded-lg text-xl items-center">
+              <span>
+                {" "}
+                এখনই কিনুন <span className="font-bold">৳১৬০</span>
+              </span>
+              <Image className="w-[22px] ml-2" src={rocket} alt="rocket" />
+            </button>
+          </div>
         </div>
       </div>
     </section>
